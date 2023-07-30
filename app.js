e7e4e223a9f93990acd719bc039ab59ad48f3b9e
Damla Oksel <damlaoksel@hotmail.com>
2023-07-30 13:45:39 +0300

    Her sey calisir durumda

--- app.js
+++ app.js
@@ -10,5 +10,26 @@ function ShowMenu(){
    console.log(isMenu);
    if(isMenu){
     //Menu icindekiler gösterilecek
+    ShowMenuItems();
+   }else{
+    //Menu icindekiler tekrar ekrana basılmasın diye icindekilerini sildik.
+    DeleteContents();
+   }
+   function ShowMenuItems(){
+    let Items=["Ana Menü","Hakkımızda","Eğitimlerimiz","Bize Ulaşın","Sosyal Medyalarımız"];
+    Items.forEach(ShowItems);
+    function ShowItems(item){
+        let liDOM=document.createElement("li");
+        liDOM.classList.add("menu-item");
+        liDOM.innerHTML=`
+            <a href="#">${item}</a>
+        `;
+        MENU.append(liDOM);
+    }
+   }
+   function DeleteContents(){
+    while (MENU.firstChild) {
+        MENU.removeChild(MENU.firstChild);
+    }
    }
 }
